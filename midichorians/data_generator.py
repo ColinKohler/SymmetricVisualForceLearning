ffb0bf84b58db8ab8d5cf0a185aa190823103331
ColinKohler <colink78@gmail.com>
2022-02-08 11:38:34 -0500

    Filling in bones for data generation.

--- midichorians/data_generator.py
+++ midichorians/data_generator.py
@@ -3,6 +3,10 @@ import torch
 import numpy as np
 import numpy.random as npr
 
+from sac_agent import SACAgent
+
+from helping_hands_rl_envs import env_factory
+
 @ray.remote
 class DataGenerator(object):
   '''
@@ -18,11 +22,13 @@ class DataGenerator(object):
     self.config = config
     self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    self.env = None
-    self.agent = None
-
     npr.seed(self.seed)
     torch.manual_seed(self.seed)
+
+    self.env = env_factory.createEnvs(0, self.config.env_type, self.config.env_config)
+
+    self.agent = SACAgent()
+    self.agent.setWeights(initial_checkpoint['weights'])
 
   def continuousDataGen(self, shared_storage, replay_buffer, test_mode=False):
     '''
@@ -38,7 +44,30 @@ class DataGenerator(object):
       ray.get(shared_storage.getInfo.remote('training_step')) < self.config.training_steps and \
       not ray.get(shared_storage.getInfo.remote('terminate'))
     ):
-      pass
+      self.agent.setWeights(ray.get(shared_storage.getInfo.remote('training_step')))
+
+      if not test_mode:
+        eps_history = self.generateEpisode(test_mode)
+        replay_buffer.add.remote(eps_history, shared_storage)
+      else:
+        eps_history = self.generateEpisode(test_mode)
+
+        past_100_rewards = ray.get(shared_storage.getInfo.remote('past_100_rewards'))
+        past_100_rewards.append(eps_history.reward_history[-1])
+
+        shared_storage.setInfo.remote(
+          {
+            'past_100_rewards' : past_100_rewards
+          }
+        )
+
+      if not test_mode and self.config.data_gen_delay:
+        time.sleep(self.config.gen_delay)
+      if not test_mode and self.config.train_data_ratio:
+        while(
+          ray.get(shared_storage.getInfo.remote('training_step')) \ max(1, ray.get(shared_storoage.getInfo.remote('num_steps'))) < self.config.train_data_ratio
+        ):
+          time.sleep(0.5)
 
   def generateEpisode(self, test_mode):
     '''
@@ -50,7 +79,18 @@ class DataGenerator(object):
     Returns:
       None : Episode history
     '''
-    pass
+    eps_history = EpisodeHistory()
+
+    obs = self.env.reset()
+    eps_history.logStep(obs, None, 0, 0)
+
+    done = False
+    while not done:
+      action, value = self.agent.selectAction(obs)
+      obs, reward, done = self.env.step(action.cpu().numpy(), auto_reset=False)
+      eps_history.logStep(obs, action, value, reward)
+
+    return eps_history
 
 class EpisodeHistory(object):
   '''
@@ -59,8 +99,14 @@ class EpisodeHistory(object):
   def __init__(self):
     self.obs_history = list()
     self.action_history = list()
-    self.reward_history = list()
     self.value_history = list()
+    self.reward_history = list()
 
     self.priorities = None
     self.eps_priority = None
+
+  def logStep(self, obs, action, value, reward):
+    self.obs_history.append(obs)
+    self.action_history.append(action)
+    self.value_history.append(value)
+    self.reward_history.append(reward)
