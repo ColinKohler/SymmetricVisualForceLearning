Fixed bug in normalization of force stack due to swtich from force magnitude to vectors. Testing smaller dpos for agent to get better force values.

configs/block_pulling_corner.py
@@ -17,10 +17,10 @@ class BlockPullingCornerConfig(Config):
     self.seed = None
 
     # Env
-    self.robot = 'kuka'
+    self.robot = 'panda'
     self.env_type = 'force_block_pulling_corner'
     self.max_steps = 100
-    self.dpos = 0.05
+    self.dpos = 0.025
     self.drot = np.pi / 8
 
     # Data Gen
@@ -78,7 +78,7 @@ class BlockPullingCornerConfig(Config):
       'action_sequence' : self.action_sequence,
       'robot' : self.robot,
       'num_objects' : 1,
-      'object_scale_range' : (1.0, 1.0),
+      'object_scale_range' : (1.2, 1.2),
       'random_orientation' : self.random_orientation,
       'workspace_check' : 'point',
       'reward_type' : self.reward_type,

midichlorians/torch_utils.py
@@ -29,7 +29,7 @@ def unnormalizeObs(obs):
   return obs / 255 * 0.4
 
 def normalizeForce(force):
-  force = np.clip(force, 0, 100)
+  force = np.clip(force, -100, 100)
   force = force / 100
   return force
 

scripts/test.py
@@ -51,20 +51,24 @@ if __name__ == '__main__':
   for i in range(args.num_eps):
     done = False
     obs = env.reset()
-    force_stack = np.zeros((4, 2))
+    force_stack = np.zeros((4, 6))
     force_stack[-1] = obs[3]
     while not done:
-      action_idx, action, value = agent.getAction(
-        [obs[0]],
-        obs[2],
-        torch_utils.normalizeForce(force_stack),
-        evaluate=True
-      )
-      #expert_action = torch.tensor(env.getNextAction()).float()
-      #action_idx, action = agent.convertPlanAction(expert_action.view(1, -1))
+      print(force_stack)
+      print(torch_utils.normalizeForce(force_stack))
+      print()
+      plt.imshow(obs[2].squeeze(), cmap='gray'); plt.show()
+      #action_idx, action, value = agent.getAction(
+      #  [obs[0]],
+      #  obs[2],
+      #  torch_utils.normalizeForce(force_stack),
+      #  evaluate=True
+      #)
+      expert_action = torch.tensor(env.getNextAction()).float()
+      action_idx, action = agent.convertPlanAction(expert_action.view(1, -1))
 
       obs, reward, done = env.step(action.cpu().squeeze().numpy(), auto_reset=False)
-      force_stack_ = np.zeros((4, 2))
+      force_stack_ = np.zeros((4, 6))
       force_stack_[:-1] = force_stack[1:]
       force_stack_[-1] = obs[3]
       force_stack = force_stack_