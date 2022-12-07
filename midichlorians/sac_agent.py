Fixed bug where max force from config was not used for force normalization

midichlorians/sac_agent.py
@@ -57,7 +57,7 @@ class SACAgent(object):
     state = torch.Tensor(state).view(len(state), 1, 1, 1).to(self.device)
     state_tile = state.repeat(1, 1, obs.size(2), obs.size(3))
     obs = torch.cat((obs, state_tile), dim=1)
-    force = torch.Tensor(np.clip(force, -30, 30) / 30).view(len(state), self.config.force_history, self.config.force_dim).to(self.device)
+    force = torch.Tensor(torch_utils.normalizeForce(force, self.config.max_force)).view(len(state), self.config.force_history, self.config.force_dim).to(self.device)
 
     with torch.no_grad():
       if evaluate: