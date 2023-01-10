d4cdadcc70c06f18d1daa81e5deb7f571f6b41c9
ColinKohler <colink78@gmail.com>
2023-01-10 17:32:06 -0500

    Updating proprio encoder to be equivariant

--- midichlorians/models/encoders/proprio_encoder.py
+++ midichlorians/models/encoders/proprio_encoder.py
@@ -5,19 +5,37 @@ import torch.nn.functional as F
 class ProprioEncoder(nn.Module):
   '''
   '''
-  def __init__(self, z_dim=64):
+  def __init__(self, z_dim=64, initialize=True, N=8):
     super().__init__()
 
-    self.fc = nn.Sequential(
-      nn.Linear(4, z_dim // 8),
-      nn.LeakyReLU(0.1, inplace=True),
-      nn.Linear(z_dim // 8, z_dim // 4),
-      nn.LeakyReLU(0.1, inplace=True),
-      nn.Linear(z_dim // 4, z_dim // 2),
-      nn.LeakyReLU(0.1, inplace=True),
-      nn.Linear(z_dim // 2, 2 * z_dim),
-      nn.LeakyReLU(0.1, inplace=True),
-    )
+    self.z_dim = z_dim
+    self.N = N
+
+    self.c4_act = gspaces.rot2dOnR2(self.N)
+    self.n_rho1 = 1
+    self.invariant_proprio_repr = 2 * [self.c4_act.trivial_repr]
+    self.equivariant_proprio_repr = self.n_rho1 * [self.c4_act.irrep(1)]
+
+    self.layers = list()
+    self.in_type = enn.FieldType(self.c4_act, self.equivariant_proprio_repr + self.invariant_proprio_repr)
+    out_type = enn.FieldType(self.c4_act, z_dim // 8 * [self.c4_act.regular_repr])
+    self.layers.append(EquivariantBlock(self.in_type, out_type, kernel_size=1, stride=1, padding=0, initialize=initialize))
+
+    in_type = out_type
+    out_type = enn.FieldType(self.c4_act, z_dim // 4 * [self.c4_act.regular_repr])
+    self.layers.append(EquivariantBlock(in_type, out_type, kernel_size=1, stride=1, padding=0, initialize=initialize))
+
+    in_type = out_type
+    out_type = enn.FieldType(self.c4_act, z_dim // 2 * [self.c4_act.regular_repr])
+    self.layers.append(EquivariantBlock(in_type, out_type, kernel_size=1, stride=1, padding=0, initialize=initialize))
+
+    in_type = out_type
+    self.out_type = enn.FieldType(self.c4_act, 2 * self.z_dim * [self.c4_act.regular_repr])
+    self.layers.append(EquivariantBlock(in_type, self.out_type, kernel_size=1, stride=1, padding=0, initialize=initialize))
+
+    self.conv = nn.Sequential(*layers)
 
   def forward(self, proprio):
-    return self.fc(proprio)
+    batch_size = proprio.size(0)
+    proprio_geo = enn.GeometricTensor(proprio.view(batch_size, -1, 1, 1), self.in_type)
+    return self.conv(proprio)
