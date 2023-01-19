0f8f42b7d198bbd0b5bd13f6bd9aa6cb7c6177e4
ColinKohler <colink78@gmail.com>
2023-01-19 13:08:12 -0500

    Updaing slurm scripts

---
+++ scripts/train_3_runs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+main() {
+  local env=$1
+  local num_sensors=$2
+  local encoder=$3
+  local results_path=$4
+
+  for j in {1..3}; do
+    sbatch scripts/train_single_gpu.sbatch $env $num_sensors $encoder $results_path
+  done
+}
+
+main $1 $2 $3 $4
