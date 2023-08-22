126a6b141505deb32a96f43f99312b5b9e8800e3
Colin Kohler <colink78@gmail.com>
2023-08-22 12:04:28 -0400

    Removing files for gh-pages website.

--- scripts/multi_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pkill -9 ray
-pipenv run python scripts/train.py peg_insertion --results_path=sm_tol_1
-pkill -9 ray
-pipenv run python scripts/train.py peg_insertion --results_path=sm_tol_2
-pkill -9 ray
-pipenv run python scripts/train.py peg_insertion --results_path=sm_tol_3
